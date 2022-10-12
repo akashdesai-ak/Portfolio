95bcc05d85611990a6cbd5aa7ab0f74339c67f28
Akash Desai <81104857+akashdesai-ak@users.noreply.github.com>
2022-10-12 14:12:20 +0530

    update

--- index.php
+++ index.php
@@ -1,21 +1,3 @@
-<?php
-if(isset($_POST['submit'])){
-    $to = "aakashdesai.ak@gmail.com";
-    $name = $_POST['name'];
-    $from = $_POST['email'];
-    $subject = $_POST['subject'];
-    $message = $_POST['message'];
-    $headers = "From : $from";
-
-    mail($to,$subject,$message,$headers);
-    echo "Mail Sent Successfully..";
-}else{
-    echo "Something went wrong.";
-}
-?>
-
-
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
