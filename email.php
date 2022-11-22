61cc769991ed0fabbc9c21f6b84572ccf0fbc06a
Akash Desai <81104857+akashdesai-ak@users.noreply.github.com>
2022-11-22 13:51:51 +0530

    update

--- email.php
+++ email.php
@@ -1,37 +1,22 @@
-<?php 
-if(isset($_POST['submit'])){
-    
-    $name = $_POST['name'];
-    $fromEmail = $_POST['email'];
-    $subjectName = $_POST['subject'];
-    $message = $_POST['message'];
+<?php
+if (isset($_POST['submit'])) {
 
+  $Name = $_POST['name'];
+  $Email = $_POST['email'];
+  $Subject = $_POST['subject'];
+  $Msg = $_POST['message'];
+
+  if (empty($Name) || empty($Email) || empty($Subject) || empty($Msg)) {
+    header('location: index.php?error ');
+  } else {
     $to = "aakashdesai.ak@gmail.com";
-    $subject = $subjectName;
-    $headers = "MIME-Version: 1.0" . "\r\n";
-    $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-    $headers .= 'From: '.$from.'<'.$fromEmail.'>' . "\r\n".'Reply-To: '.$fromEmail."\r\n" . 'X-Mailer: PHP/' . phpversion();
-    $message = '<!doctype html>
-			<html lang="en">
-			<head>
-				<meta charset="UTF-8">
-				<meta name="viewport"
-					  content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-				<meta http-equiv="X-UA-Compatible" content="ie=edge">
-				<title>Document</title>
-			</head>
-			<body>
-			<span class="preheader" style="color: transparent; display: none; height: 0; max-height: 0; max-width: 0; opacity: 0; overflow: hidden; mso-hide: all; visibility: hidden; width: 0;">'.$message.'</span>
-				<div class="container">
-                 '.$message.'<br/>
-                    Regards<br/>
-                  '.$fromEmail.'
-				</div>
-			</body>
-			</html>';
-    $result = @mail($to, $subject, $message, $headers);
 
-    echo '<script>alert("Email sent successfully !")</script>';
-    // echo '<script>window.location.href="index.html";</script>';
+    if (mail($to, $Subject, $Msg, $Email)) {
+
+      header('location: index.php?success');
+    }
+  }
 }
-?>
+else{
+  header('location: index.php');
+}
